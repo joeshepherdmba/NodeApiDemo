46273ee089de10ac6d2eea06597371e32ad1f9b9
Joe Shepherd <joshephe@microsoft.com>
2018-12-05 13:32:17 -0600

    updatwed index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express-dev' });
+  res.render('index', { title: 'Express-dev-for City of Boulder' });
 });
 
 module.exports = router;
